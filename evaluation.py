5bf751c9a7045147d813040e364ed3ec9e991ae7
stefbil <32976511+stefbil@users.noreply.github.com>
2025-11-18 20:45:42 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -11,7 +11,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # GOOGLE SHEETS CONFIG
-SHEET_ID = os.environ.get("SHEET_ID")
+SHEET_ID = "1ej1aYS1Ld5tvAMQpwXZA5wwwjzCi6roHaPP5NH51wTc"
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -211,5 +211,6 @@ elif st.session_state.phase == "rating":
     render_rating()
 elif st.session_state.phase == "done":
     render_done()
+
 
 
